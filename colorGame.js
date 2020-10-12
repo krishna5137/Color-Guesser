766848f3a0cb9f0a3ab84a02915d251ee493c497
Krishna <inbox.krishna95@gmail.com>
2020-10-12 13:34:57 +0530

    add functionality to include randomness is color generation

--- colorGame.js
+++ colorGame.js
@@ -1,15 +1,8 @@
-let colors = [
-    "rgb(255, 0, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(255, 0, 255)",
-];
+let colors = generateRandomColors(6);
 
 let squares = document.getElementsByClassName("square");
 let targetColor = document.getElementById("targetColor");
-let pickedColor = colors[4];
+let pickedColor = pickRandomColor(colors);
 let displayMessage = document.querySelector("#displayMessage");
 let heading = document.querySelector("h1");
 
@@ -37,4 +30,31 @@ function changeColor(color) {
     for (var i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor = color;
     }
+}
+
+function pickRandomColor (array) {
+    //pick a random number between [0, 6) 6 is exclusive
+    let randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+}
+
+/**
+ * fills colors array with random rgb values
+ */
+function generateRandomColors(number) {
+    let arr = [];
+    for (var i = 0; i < number; i++) {
+        arr.push(generateElements());
+    }  
+    return arr;
+}
+
+/**
+ * generates random rgb values
+ */
+function generateElements () {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);  
+    return "rgb" + "(" + r + ", " + g + ", " + b + ")";
 }
